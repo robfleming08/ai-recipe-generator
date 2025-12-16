71754e7f6b859c5bcedbd34860bfe8f657de19fe
rob <robertfleming@unr.edu>
2025-12-16 03:58:53 -0500

    connect to bedrock

---
+++ amplify/backend.ts
@@ -0,0 +1,32 @@
+import { defineBackend } from "@aws-amplify/backend";
+import { data } from "./data/resource";
+import { PolicyStatement } from "aws-cdk-lib/aws-iam";
+import { auth } from "./auth/resource";
+
+const backend = defineBackend({
+  auth,
+  data,
+});
+
+const bedrockDataSource = backend.data.resources.graphqlApi.addHttpDataSource(
+  "bedrockDS",
+  "https://bedrock-runtime.us-east-1.amazonaws.com",
+  {
+    authorizationConfig: {
+      signingRegion: "us-east-1",
+      signingServiceName: "bedrock",
+    },
+  }
+);
+
+bedrockDataSource.grantPrincipal.addToPrincipalPolicy(
+  new PolicyStatement({
+    resources: [
+      "arn:aws:bedrock:us-east-1::foundation-model/*",
+    ],
+    actions: [
+      "bedrock:InvokeModel",
+      "bedrock:InvokeModelWithResponseStream",
+    ],
+  })
+);
